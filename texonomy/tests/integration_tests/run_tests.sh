433d89016596849e7996eeb987d3aa6d26c7b974
Ivy Basseches <imb2139@columbia.edu>
2023-03-02 03:42:21 -0500

    Add coverage for int tests

--- texonomy/tests/integration_tests/run_tests.sh
+++ texonomy/tests/integration_tests/run_tests.sh
@@ -6,9 +6,13 @@ reset="$(tput sgr0)"
 
 for file in *.py; do
     echo "${green}${bold}Running test: ${file%.py}...${reset}"
-    python $file > /dev/null
+    coverage run $file > /dev/null
     echo "Done."
     echo "==================="
+    echo
+    coverage report
+    echo
+    echo
 done
 
 mkdir pdf
